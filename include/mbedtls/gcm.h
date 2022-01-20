f24bab193cb94ac36326dbfcc7532f57a9438771
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2022-01-20 14:04:11 +0100

    [nrf noup] Check if GCM_C is defined in gcm.h
    -This checks if GCM_C is enabled in gcm.h
     before including the functions. This was
     causing build issues when the GCM is disabled
     but GCM_ALT is enabled.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    (cherry picked from commit 08e91487b1f490c4f66f941dae05e3a433f1b908)
    (cherry picked from commit 125633d665a8335c684ffe3a284512276bf6e86c)
    (cherry picked from commit 8ffbcc11f708ba1b7536b90901e7a93e894b26e1)
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- include/mbedtls/gcm.h
+++ include/mbedtls/gcm.h
@@ -52,6 +52,8 @@
 extern "C" {
 #endif
 
+#if defined(MBEDTLS_GCM_C)
+
 #if !defined(MBEDTLS_GCM_ALT)
 
 /**
@@ -373,10 +375,10 @@ void mbedtls_gcm_free(mbedtls_gcm_context *ctx);
 int mbedtls_gcm_self_test(int verbose);
 
 #endif /* MBEDTLS_SELF_TEST */
+#endif /* MBEDTLS_GCM_C */
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif /* gcm.h */
