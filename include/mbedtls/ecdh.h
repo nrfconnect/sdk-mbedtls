9b994d0aa8002ad68cde52fdbabbcd55c85c7da2
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-06-08 15:07:26 +0200

    Revert "[nrf noup] Disable turning of legacy context for ECDH"
    This reverts commit 5224621efdc4576e5bbc59dacd36941d0dd9b5d3.

--- include/mbedtls/ecdh.h
+++ include/mbedtls/ecdh.h
@@ -53,8 +53,7 @@
 #if defined(MBEDTLS_ECP_RESTARTABLE)
 #define MBEDTLS_ECDH_LEGACY_CONTEXT
 #else
-// mbed TLS 3.0.0 prototype: Disable legacy context being unset here
-//#undef MBEDTLS_ECDH_LEGACY_CONTEXT
+#undef MBEDTLS_ECDH_LEGACY_CONTEXT
 #endif
 
 #if defined(MBEDTLS_ECDH_VARIANT_EVEREST_ENABLED)
