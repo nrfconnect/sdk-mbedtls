f6bfe1a10d2a01e1ff1853ebc08b7aa7bec10db4
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-02-17 15:46:53 +0100

    [nrf toup] Fix RSA dependency on PK write when enabling PSA crypto
    Fix RSA (MBEDTLS_RSA_C) dependency on PK write (MBEDTLS_PK_WRITE_C)
    when enabling PSA Crypto (MBEDTLS_PSA_CRYPTO_C).
    This should instead depend on MBedTLS using PSA crypto
    (MBEDTLS_USE_PSA_CRYPTO).
    
    Raised as issue to MbedTLS project:
    https://github.com/Mbed-TLS/mbedtls/issues/7126
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    (cherry picked from commit 65311cf9ddbcdcb89254589c99fe4a921f3350b9)
    (cherry picked from commit 702f824e4608b7afb00e5883dc2c7ff9c70abe4d)
    (cherry picked from commit 45374c0380fe1df018bbad8c8eaf9a40418a66e1)
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- library/pk_wrap.c
+++ library/pk_wrap.c
@@ -295,7 +295,7 @@ static int rsa_verify_wrap(void *ctx, mbedtls_md_type_t md_alg,
 }
 #endif
 
-#if defined(MBEDTLS_PSA_CRYPTO_C)
+#if defined(MBEDTLS_USE_PSA_CRYPTO)
 int  mbedtls_pk_psa_rsa_sign_ext(psa_algorithm_t alg,
                                  mbedtls_rsa_context *rsa_ctx,
                                  const unsigned char *hash, size_t hash_len,
